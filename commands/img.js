13a088b539df96ae28ea97cf19d012cbfe30b284
lkassasin9-cmyk <lkassasin9@gmail.com>
2026-08-19 13:47:24 +0000

    Update img.js

--- commands/img.js
+++ commands/img.js
@@ -57,7 +57,7 @@ async function img(message, client) {
                     caption:
                         `📷 ${query}\n` +
                         `${image.title && image.title !== "No title" ? image.title + "\n" : ""}` +
-                        `© Digital Crew 243`
+                        `© LK PRICE 226`
                 });
 
                 await new Promise(r => setTimeout(r, 1000));
